712d50ee29f175723be04a707d34c2da1eead717
rizaradiarivaldo <rizaradiarivaldo@gmail.com>
2024-07-17 06:45:34 +0700

    Initial Commit

---
+++ PokeCard/PokeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeCardApp.swift
+//  PokeCard
+//
+//  Created by Riza Radia Rivaldo on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
